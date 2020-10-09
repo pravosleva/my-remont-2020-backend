87c08dfc282102e24b03b4b23be4490dd1cd662b
Denis Poltoratsky <dpoltoratskiy@igatec.com>
2020-10-10 02:28:30 +0300

    socket added

--- api/remont/models/remont.js
+++ api/remont/models/remont.js
@@ -5,4 +5,39 @@
  * to customize this model
  */
 
-module.exports = {};
+function debounce(f, ms) {
+  let timer = null;
+
+  return function (...args) {
+    const onComplete = () => {
+      f.apply(this, args);
+      timer = null;
+    };
+
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(onComplete, ms);
+  };
+}
+
+const debouncedAfterUpdate = debounce(({ result, params, data }) => {
+  // console.log("=== debounced socket.emit for afterUpdate webhook");
+  // strapi.io.emit("ARTICLE_UPDATED", {
+  //   id: model._conditions ? model._conditions._id : null,
+  // });
+  strapi.io.emit("REMONT_UPDATED", { result, params, data })
+}, 4000);
+
+module.exports = {
+  /**
+   * Triggered before user creation.
+   */
+  lifecycles: {
+    async afterUpdate(result, params, data) {
+      // SAMPLE:
+      // const passwordHashed = await strapi.api.user.services.user.hashPassword(data.password);
+      // data.password = passwordHashed;
+
+      debouncedAfterUpdate({ result, params, data });
+    },
+  },
+};
--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -1,4 +1,5 @@
 'use strict';
+const geoip = require('geoip-lite');
 
 /**
  * An asynchronous bootstrap function that runs before
@@ -10,4 +11,62 @@
  * See more details here: https://strapi.io/documentation/v3.x/concepts/configurations.html#bootstrap
  */
 
-module.exports = () => {};
+function isJsonString(str) {
+  try {
+    JSON.parse(str);
+  } catch (e) {
+    return false;
+  }
+  return true;
+}
+
+const getObjectByMap = (map) => {
+  const result = Object.create(null)
+
+  map.forEach((value, key, map) => {
+    if (value instanceof Map) {
+      result[key] = getObjectByMap(value)
+    } else {
+      if (isJsonString(JSON.stringify(value))) {
+        result[key] = { ...value }
+      } else {
+        result[key] = value
+      }
+    }
+  })
+  return result
+}
+
+const users = new Map()
+
+module.exports = () => {
+  var io = require('socket.io')(strapi.server)
+
+  // Listen for user connection
+  io.on('connection', function(socket){
+    socket.emit('YOURE_WELCOME')
+    const clientIp = socket.request.connection.remoteAddress
+    const clientId = socket.id
+
+    console.log(socket.request.connection._peername.address)
+    console.log(socket.ipAddress)
+
+    users.set(clientId, { ip: clientIp, geo: geoip.lookup(clientIp) })
+    socket.broadcast.emit('SOMEBODY_CONNECTED', { msg: `${clientId} connected, his remoteAddress= ${socket.request.connection.remoteAddress}, ip= ${socket.request.connection._peername.address}`, users: getObjectByMap(users) })
+
+    // Send message on user connection
+    socket.emit('HELLO', { msg: `Hello, your ID is ${clientId}, your IP is ${clientIp}`, users: getObjectByMap(users) })
+
+    socket.on('reconnect', (attemptNumber) => {
+      socket.broadcast.emit('SOMEBODY_RECONNECTED', { msg: `${clientId} reconnected ${attemptNumber}, has users= ${users.has(clientId)}`, users: getObjectByMap(users) })
+    });
+    // socket.on('TEST', () => { socket.emit('TESTED', { msg: 'hw' }) })
+    // Listen for user diconnect
+    socket.on('disconnect', (socket) => {
+      console.log(socket)
+      users.delete(clientId)
+      io.emit('SOMEBODY_DISCONNECTED', { msg: `${clientId} disconnected`, users: getObjectByMap(users) })
+    })
+  })
+  strapi.io = io // register socket io inside strapi main object to use it globally anywhere
+};
