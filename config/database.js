73512a911603704b42935dc9d03e4c6a381f6f33
Denis Poltoratsky <dpoltoratskiy@igatec.com>
2020-11-27 22:15:25 +0300

    connection cfg updated

--- config/database.js
+++ config/database.js
@@ -5,15 +5,15 @@ module.exports = ({ env }) => ({
       connector: 'mongoose',
       settings: {
         host: env('DATABASE_HOST', env.DATABASE_HOST),
-        srv: env.bool('DATABASE_SRV', false),
+        srv: env.bool('DATABASE_SRV', true),
         port: env.int('DATABASE_PORT', env.DATABASE_PORT),
         database: env('DATABASE_NAME', env.DATABASE_NAME),
         username: env('DATABASE_USERNAME', env.DATABASE_USERNAME),
         password: env('DATABASE_PASSWORD', env.DATABASE_PASSWORD),
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', env.AUTHENTICATION_DATABASE),
-        ssl: env.bool('DATABASE_SSL', false),
+        // authenticationDatabase: env('AUTHENTICATION_DATABASE', env.AUTHENTICATION_DATABASE),
+        ssl: env.bool('DATABASE_SSL', true),
       },
     },
   },
